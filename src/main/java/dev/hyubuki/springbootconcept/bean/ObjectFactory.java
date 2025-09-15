ff4e23abe382c495d426963cb5517d13be10ac42
hyubuki <suhyuk.ts@gmail.com>
2025-09-15 14:29:39 +0900

    [feat]: introduce spring container(IoC/DI) using BeanFactory

--- src/main/java/dev/hyubuki/springbootconcept/bean/ObjectFactory.java
+++ src/main/java/dev/hyubuki/springbootconcept/bean/ObjectFactory.java
@@ -2,16 +2,22 @@ package dev.hyubuki.springbootconcept.bean;
 
 import dev.hyubuki.springbootconcept.payment.ExRateProvider;
 import dev.hyubuki.springbootconcept.payment.SimpleExRateProvider;
-import dev.hyubuki.springbootconcept.payment.WebApiExRatePaymentProvider;
 import dev.hyubuki.springbootconcept.payment.service.PaymentService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class ObjectFactory {
 
+  @Bean
   public PaymentService paymentService() {
     return new PaymentService(exRateProvider());
   }
 
+  @Bean
   public ExRateProvider exRateProvider() {
-    return new WebApiExRatePaymentProvider();
+//    return new WebApiExRatePaymentProvider();
+    return new SimpleExRateProvider();
   }
 }
--- src/main/java/dev/hyubuki/springbootconcept/client/Client.java
+++ src/main/java/dev/hyubuki/springbootconcept/client/Client.java
@@ -6,14 +6,16 @@ import dev.hyubuki.springbootconcept.payment.entity.Payment;
 import dev.hyubuki.springbootconcept.payment.service.PaymentService;
 import java.io.IOException;
 import java.math.BigDecimal;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
 
   public static void main(String[] args) throws IOException {
-    ObjectFactory objectFactory = new ObjectFactory();
-    PaymentService service = objectFactory.paymentService();
-    Payment payment = service.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+    BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+    PaymentService service = beanFactory.getBean(PaymentService.class);
 
+    Payment payment = service.prepare(100L, "USD", BigDecimal.valueOf(50.7));
     System.out.println(payment.toString());
   }
 }
