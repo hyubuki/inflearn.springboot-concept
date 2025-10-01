1652f4d2f3a326162afc79d45556161e81b87ffe
hyubuki <suhyuk.ts@gmail.com>
2025-10-01 10:46:34 +0900

    [refactor]: replace URLConnection with HttpClient

--- src/main/java/dev/hyubuki/springbootconcept/exrate/WebApiExRatePaymentProvider.java
+++ src/main/java/dev/hyubuki/springbootconcept/exrate/WebApiExRatePaymentProvider.java
@@ -2,9 +2,13 @@ package dev.hyubuki.springbootconcept.exrate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.hyubuki.springbootconcept.api.ApiTemplate;
-import dev.hyubuki.springbootconcept.api.SimpleApiExecutor;
 import dev.hyubuki.springbootconcept.payment.ExRateProvider;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import org.apache.commons.lang3.exception.UncheckedException;
 
 public class WebApiExRatePaymentProvider implements ExRateProvider {
 
@@ -15,11 +19,25 @@ public class WebApiExRatePaymentProvider implements ExRateProvider {
 
       String url = "https://open.er-api.com/v6/latest/" + currency;
       // Strategy パターン　method injection - callback
-      return apiTemplate.getExRate(url, new SimpleApiExecutor(), (response) -> {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData exRateData = mapper.readValue(response, ExRateData.class);
-        return exRateData.rates().get("KRW");
-      });
+      return apiTemplate.getExRate(url, (uri) -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+
+            try {
+              try (HttpClient client = HttpClient.newBuilder().build()) {
+                return client.send(request, BodyHandlers.ofString()).body();
+              }
+            } catch (InterruptedException | IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+          , (response) -> {
+            ObjectMapper mapper = new ObjectMapper();
+            ExRateData exRateData = mapper.readValue(response, ExRateData.class);
+            return exRateData.rates().get("KRW");
+          });
     }
   }
 }
